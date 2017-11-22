e68f65a6326f2e4d03397faa8dc82c12dee2ba69
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-21 18:04:22 -0800

    Midway through Auth cycle, currently creating greeting container

---
+++ frontend/components/greeting/greeting_container.js
@@ -0,0 +1,24 @@
+import {connect} from 'react-redux';
+import {logout} from '../../actions/session_actions';
+import Greeting from './greeting'
+
+const mapStateToProps = state => {
+  // debugger; This is how you would test and see your state. But you must
+  // remove the normal parentheses and add the curly braces instead, allowing you
+  // to write in debugger; but remember you must manually return in this case!
+  return {
+  currentUser: state.session.currentUser
+}};
+// Identical to below:
+// const mapStateToProps = ({ session }) => ({
+//   currentUser: session.currentUser
+// });
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Greeting);
