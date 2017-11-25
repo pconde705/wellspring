07e925abdbae161a942d0fe3b8cd5514db6e72b6
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-25 12:28:15 -0800

    Updated file structure

--- frontend/components/project_index/project_index.jsx
+++ frontend/components/project_index/project_index.jsx
@@ -9,18 +9,37 @@ class ProjectIndex extends React.Component {
   constructor(props) {
     super(props)
     // console.log(props); // this console.log is run before componentDidMount so projects will be empty
+    this.state = {projects: []}
+    // this.renderProjects = this.renderProjects.bind(this);
   }
 
   componentDidMount(){ // this function is needed or else this.props.projects will render a blank array/object
     this.props.fetchAllProjects(); //this comes from the dispatch action
   }
 
+  renderProjects(e) {
+    e.preventDefault();
+    console.log("Hello");
+    return (
+      <div>
+        <p>Hello</p>
+        <ul>
+          {
+            this.props.projects.map((project, key) => (
+              <li key={key}><h2>{project.title}</h2></li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+ // Router
   render() {
     return (
       <div>
         <div className="category-bar-div">
           <ul>
-            <li><Link to="/">Music</Link></li>
+            <li><button onClick={this.renderProjects}>Music</button></li>
             <li><Link to="/">Comics & Illustration</Link></li>
             <li><Link to="/">Film</Link></li>
             <li><Link to="/">Food & Craft</Link></li>
@@ -30,14 +49,6 @@ class ProjectIndex extends React.Component {
             <li><Link to="/">Arts</Link></li>
           </ul>
         </div>
-        <ul>
-        {
-          this.props.projects.map((project, key) => (
-            <li key={key}><h2>{project.title}</h2>
-            <img src={project.main_image_url}/></li>
-          ))
-        }
-        </ul>
       </div>
     )
   }
