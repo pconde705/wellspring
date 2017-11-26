8740abd775f4cf94e21fea0f10959e49a370ffca
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-26 13:58:30 -0800

    Fixed text wrapping with image

--- frontend/components/project_index/project_index_display.jsx
+++ frontend/components/project_index/project_index_display.jsx
@@ -15,9 +15,11 @@ class ProjectIndexDisplay extends React.Component {
         {filteredProjects.map((project, key) => (
           <div className="featured-project-div" key={key}>
             <img src={project.main_image_url} />
-            <h1 className="featured-title">{project.title}</h1>
-            <p className="featured-by">BY {project.user}</p>
-            <p className="featured-percent">{(100 / project.goal) * project.money_raised}% FUNDED</p>
+            <div className="featured-names">
+              <h1 className="featured-title">{project.title}</h1>
+              <p className="featured-by">BY {project.user}</p>
+              <p className="featured-percent">{(100 / project.goal) * project.money_raised}% FUNDED</p>
+            </div>
           </div>
         ))}
       </div>
