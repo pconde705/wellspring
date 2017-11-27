be3c2715f34eef157ea848c69acd798c90ba4271
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-26 19:44:19 -0800

    Started work on completion line

--- frontend/components/project_show/project_show.jsx
+++ frontend/components/project_show/project_show.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 class ProjectShow extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.setLine = this.setLine.bind(this)
+  }
 
   componentDidMount() {
     this.props.fetchSingleProject(this.props.match.params.id)
+  }
+
+  setLine() {
+    let bar_width = (100 / this.props.project.undefined.projects.goal) * this.props.project.undefined.projects.money_raised
+    if (bar_width > 100) {
+      return {width: "100px"}
+    } else {
+      return {width: `${bar_width}%`}
+    }
   }
 
   render () {
@@ -28,15 +42,24 @@ class ProjectShow extends React.Component {
               <img src={this.props.project.undefined.projects.main_image_url} />
               <div className="show-goals-only">
                 <div className="show-goals-numbers-only">
-
+                  <div className="show-lines">
+                    <div className="show-completion-line" style={this.setLine()}></div>
+                    <div className="show-grey-line"></div>
+                  </div>
+                  <h2>${this.props.project.undefined.projects.money_raised}</h2>
+                  <p>pledged of ${this.props.project.undefined.projects.goal} goal</p>
+                  <h2>{this.props.project.undefined.projects.backers}</h2>
+                  <p>backers</p>
+                  <h2>29</h2>
+                  <p>days to go</p>
                 </div>
-                <button></button>
-                <p>All or nothing.</p>
+                <button className="show-backer-button">Back this project</button>
               </div>
             </div>
           </div>
           <div className="show-bottom-half">
             <div className="show-description">
+              <p>{this.props.project.undefined.projects.description}</p>
               <img src={this.props.project.undefined.projects.extra_image_url} />
             </div>
             <div className="show-backer-rewards">
