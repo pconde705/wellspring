fa9e4fe91328d518ee74dcb582397c430265a6b7
Patrick Conde <patrick.conde.costas@gmail.com>
2017-11-27 18:34:51 -0800

    Started adding error functionality to backers

--- frontend/actions/reward_actions.js
+++ frontend/actions/reward_actions.js
@@ -4,6 +4,19 @@ import {postRewards} from '../util/rewards_api_util';
 export const RECEIVE_REWARD = 'RECEIVE_REWARD';
 
 
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
+export const RECEIVE_REWARD_ERRORS = 'RECEIVE_REWARD_ERRORS';
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
+const receiveProjectErrors = (errors) => ({
+  type: RECEIVE_REWARD_ERRORS,
+  errors
+});
+
+
 const receiveReward = (reward) => ({
   type: RECEIVE_REWARD,
   reward
--- frontend/components/reward/reward.jsx
+++ frontend/components/reward/reward.jsx
@@ -14,7 +14,7 @@ class Reward extends React.Component {
     this.props.createReward(this.props.match.params.project_id, reward).then(
       this.setState({
         amount: 0, reward_subtitle: "", reward_description: "", includes: "", reward_backers: 0, reward_date: "Dec 2018"
-      })
+      }), this.props.history.push(`/projects/${this.props.match.params.project_id}`)
     );
   }
 
